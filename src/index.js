f10870d55d870124bbd2412d5614f2372256c290
Litvinov Alexander <bumble-bee83@mail.ru>
2018-09-18 23:43:28 +0300

    quadratic-equation: exec task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let valuetArray = equation.match(/-?\s?\d+/g);;
+  let a = parseInt(valuetArray[0]);
+  let b = parseInt(valuetArray[2].replace(' ', ''));
+  let c = parseInt(valuetArray[3].replace(' ', ''));
+  let resultArray = [];
+  resultArray[0] = Math.round((-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+  resultArray[1] = Math.round((-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a));
+  return resultArray.sort((left, right) => {return left - right});
 }
